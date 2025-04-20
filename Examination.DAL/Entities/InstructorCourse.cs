0462f2485117964f8a7401d71b858b10f56564de
Sara Salem <sarasalem9898@outlook.com>
2025-04-20 16:28:02 +0200

    add migration addAddtionalDataToinstructorCourseTbl

--- Examination.DAL/Entities/InstructorCourse.cs
+++ Examination.DAL/Entities/InstructorCourse.cs
@@ -13,6 +13,15 @@ public partial class InstructorCourse
 
     public int? DepartmentBranchId { get; set; }
 
+    public int? TotalStudents { get; set; }
+
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string? LastGeneratedExamType { get; set; } 
+
+    public int? FinalPassedStudentCount { get; set; }
+    public int? CorrectivePassedStudentCount { get; set; }
+
     public virtual Course Course { get; set; } = null!;
 
     public virtual DepartmentBranch? DepartmentBranch { get; set; }
---
+++ Examination.DAL/Migrations/20250420142652_addAddtionalDataToInstructorCourseTbl.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examination.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAddtionalDataToInstructorCourseTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectivePassedStudentCount",
+                table: "Instructor_Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Instructor_Courses",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FinalPassedStudentCount",
+                table: "Instructor_Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastGeneratedExamType",
+                table: "Instructor_Courses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Instructor_Courses",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalStudents",
+                table: "Instructor_Courses",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectivePassedStudentCount",
+                table: "Instructor_Courses");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Instructor_Courses");
+
+            migrationBuilder.DropColumn(
+                name: "FinalPassedStudentCount",
+                table: "Instructor_Courses");
+
+            migrationBuilder.DropColumn(
+                name: "LastGeneratedExamType",
+                table: "Instructor_Courses");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Instructor_Courses");
+
+            migrationBuilder.DropColumn(
+                name: "TotalStudents",
+                table: "Instructor_Courses");
+        }
+    }
+}
