63b41d933ba9e6c7edfc5b4f6b14a55ca8a1808a
Sara Salem <sarasalem9898@outlook.com>
2025-04-20 16:00:39 +0200

    add update ExamQuestion  in admin area

--- Examination.PL/BL/ExamQuestionService.cs
+++ Examination.PL/BL/ExamQuestionService.cs
@@ -12,11 +12,13 @@ namespace Examination.PL.BL
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly ILogger<ExamQuestionService> _logger;
-        public ExamQuestionService(IUnitOfWork unitOfWork, IMapper mapper ,ILogger<ExamQuestionService> logger)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ExamQuestionService(IUnitOfWork unitOfWork, IMapper mapper ,ILogger<ExamQuestionService> logger,IHttpContextAccessor httpContextAccessor)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
 
         }
         public PaginatedData<ExamQuestionMV> GetAllExamsQuestions(ExamQuestionSearchMV searchMV, int PageSize = 7, int Page = 1)
@@ -55,6 +57,97 @@ namespace Examination.PL.BL
                 return null;
             }
         }
+
+        public int Add(ExamQuestionMV model)
+        {
+            var result = 0;
+            try
+            {
+                ExamQ examQ = _mapper.Map<ExamQ>(model);
+                examQ.CreatedAt = DateTime.Now;
+                examQ.Status = (int)Status.Active;
+                examQ.CreatedBy = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst("UserId")?.Value);
+                _unitOfWork.ExamQuestionRepo.Insert(examQ);
+                result= _unitOfWork.Save();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "error occuired while adding new exam question");
+                return 0;
+            }
+        }
+        public int Update(ExamQuestionMV model)
+        {
+            var result = 0;
+            try
+            {
+                ExamQ examQ = _unitOfWork.ExamQuestionRepo.GetById(model.Id);
+                if (examQ == null)
+                {
+                    return 0;
+                }
+                examQ.Question = model.Question;
+                examQ.QuestionType = model.QuestionType;
+                examQ.Answers = model.Answers;
+                examQ.RightAnswer = model.RightAnswer;
+                examQ.Degree = model.Degree;
+                examQ.ExamId = model.ExamId;
+                examQ.UpdatedAt = DateTime.Now;
+                examQ.UpdatedBy = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst("UserId")?.Value);
+                _unitOfWork.ExamQuestionRepo.Update(examQ);
+                result= _unitOfWork.Save();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "error occuired while updating exam question");
+                return 0;
+            }
+        }
+        public ExamQuestionMV GetById(int id)
+        {
+            try
+            {
+                var examQ = _unitOfWork.ExamQuestionRepo.GetById(id);
+                if (examQ == null)
+                {
+                    return null;
+                }
+                var result = _mapper.Map<ExamQuestionMV>(examQ);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "error occuired while getting exam question by id");
+                return null;
+
+            }
+        }
+        public int ChangeStatus(int id, int status)
+        {
+            try
+            {
+                var examQ = _unitOfWork.ExamQuestionRepo.GetById(id);
+                if (examQ == null)
+                {
+                    return 0;
+                }
+                examQ.Status = status;
+                examQ.UpdatedAt = DateTime.Now;
+                examQ.UpdatedBy = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst("UserId")?.Value);
+                _unitOfWork.ExamQuestionRepo.Update(examQ);
+                var result = _unitOfWork.Save();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "error occuired while changing status of exam question");
+                return 0;
+            }
+        }
+
+
 
 
     }
--- Examination.PL/IBL/IExamQuestionService.cs
+++ Examination.PL/IBL/IExamQuestionService.cs
@@ -5,6 +5,10 @@ namespace Examination.PL.IBL
     public interface IExamQuestionService
     {
        public PaginatedData<ExamQuestionMV> GetAllExamsQuestions(ExamQuestionSearchMV searchMV, int PageSize = 10, int Page = 1);
+       public int Add(ExamQuestionMV model);
+       public int Update(ExamQuestionMV model);
+       public ExamQuestionMV GetById(int id);
+       public int ChangeStatus(int id, int status);
 
 
     }
