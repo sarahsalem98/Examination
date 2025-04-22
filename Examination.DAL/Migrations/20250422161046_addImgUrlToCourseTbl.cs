3a9c8ed7d7ceea5ddbf51c23c0f9a1654712caca
Sara Salem <sarasalem9898@outlook.com>
2025-04-22 18:13:50 +0200

    add imgurl to course Entity

---
+++ Examination.DAL/Migrations/20250422161046_addImgUrlToCourseTbl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examination.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addImgUrlToCourseTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "Courses");
+        }
+    }
+}
