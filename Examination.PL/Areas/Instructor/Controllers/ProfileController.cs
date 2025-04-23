c3a4837f1c1e4f515fc1f0e83ba1b896c8fbe697
Yomna Hatem <yomnaahateem2001@gmail.com>
2025-04-23 23:10:41 +0200

    v3

---
+++ Examination.PL/Areas/Instructor/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Examination.PL.Areas.Instructor.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
