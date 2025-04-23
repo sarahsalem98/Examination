edf71277df6f92ab643c7a92c944ca2333a38c76
Yomna Hatem <yomnaahateem2001@gmail.com>
2025-04-23 22:30:47 +0200

    comments removed

--- Examination.PL/Areas/Student/Controllers/ProfileController.cs
+++ Examination.PL/Areas/Student/Controllers/ProfileController.cs
@@ -18,13 +18,7 @@ namespace Examination.PL.Areas.Student.Controllers
 
         public IActionResult Index()
         {
-            //int userId = int.Parse(User.FindFirst("UserId")?.Value ?? "0");
-            //var student = _studentService.GetProfile(userId);
-
-            //if (student == null)
-            //    return NotFound();
-
-            //return View(student);
+          
 
             var userId = int.Parse(User.FindFirst("UserId").Value);
             var student = _studentService.GetProfile(userId);
@@ -32,39 +26,7 @@ namespace Examination.PL.Areas.Student.Controllers
         }
 
 
-        //  [HttpPost]
-        //public IActionResult UpdateProfile([FromBody] StudentMV student)
-        //{
-        //    if (!ModelState.IsValid)
-        //        return BadRequest(new { success = false, message = "Invalid data", errors = ModelState });
 
-        //    var result = _studentService.UpdateProfile(student);
-
-        //    if (result <= 0)
-        //        return Json(new { success = false, message = "Update failed inside service" });
-
-        //    return Json(new { success = true });
-        //}
-        //        [HttpPost]
-        //public IActionResult UpdateProfile(StudentMV student)
-        //{
-        //    if (!ModelState.IsValid)
-        //        return Json(new { success = false, message = "Invalid data" });
-
-        //    var result = _studentService.UpdateProfile(student);
-        //    return Json(new { success = result > 0 });
-        //}
-
-        //[HttpPost]
-        //public IActionResult UpdateProfile([FromBody] StudentMV student)
-        //{
-        //    if (!ModelState.IsValid)
-        //        return Json(new { success = false, message = "Invalid data" });
-
-        //    var result = _studentService.UpdateProfile(student);
-
-        //    return Json(new { success = result > 0 });
-        //}
         [HttpPost]
         public IActionResult UpdateProfile([FromBody] StudentUpdateProfileMV student)
         {
