987ed6561b3406a6b119a43237ef83c0eb74cd65
Sara Salem <sarasalem9898@outlook.com>
2025-04-27 12:02:44 +0300

    change dbContext

--- Examination.DAL/Repos/UnitOfWork.cs
+++ Examination.DAL/Repos/UnitOfWork.cs
@@ -13,37 +13,48 @@ namespace Examination.DAL.Repos
     public class UnitOfWork : IUnitOfWork
     {
         private readonly AppDbContext _db;
-        public IStudentRepo StudentRepo { get { return new StudentRepo(_db); } }
-        public IUserRepo UserRepo { get { return new UserRepo(_db); } }
-        public IBranchRepo BranchRepo { get { return new BranchRepo(_db); } }
-        public IDepartmentRepo DepartmentRepo { get { return new DepartmentRepo(_db); } }
-        public IInstructorRepo InstructorRepo { get { return new InstructorRepo(_db); } }
-        public IUserTypeRepo UserTypeRepo { get { return new UserTypeRepo(_db); } }
-        public IExamRepo ExamRepo { get { return new ExamRepo(_db); } }
-        public IDepartmentBranch DepartmentBranchRepo { get { return new DepartmentBranchRepo(_db); } }
-        public ICourseRepo CourseRepo { get { return new CourseRepo(_db); } }
-        public IExamQuestionRepo ExamQuestionRepo { get { return new ExamQuestionRepo(_db); } }
-        public IGeneratedExamQRepo GeneratedExamQRepo { get { return new GeneratedExamQRepo(_db); } }
-        public IInstructorCourseRepo InstructorCourseRepo { get { return new InstructorCourseRepo(_db); } }
-        public IGeneratedExamRepo GeneratedExamRepo { get { return new GeneratedExamRepo(_db); } }
-        public ICourseDepartmentRepo CourseDepartmentRepo { get { return new CourseDepartmentRepo(_db); } }
-        public ITopicRepo TopicRepo { get { return new TopicRepo(_db); } }
-        public ICourseTopicRepo CourseTopicRepo { get { return new CourseTopicRepo(_db); } }
-        public IStudentCourseRepo StudentCourseRepo { get { return new StudentCourseRepo(_db); } }
+
+
+        private IStudentRepo _studentRepo;
+        private IUserRepo _userRepo;
+        private IBranchRepo _branchRepo;
+        private IDepartmentRepo _departmentRepo;
+        private IInstructorRepo _instructorRepo;
+        private IUserTypeRepo _userTypeRepo;
+        private IExamRepo _examRepo;
+        private IDepartmentBranch _departmentBranchRepo;
+        private ICourseRepo _courseRepo;
+        private IExamQuestionRepo _examQuestionRepo;
+        private IGeneratedExamQRepo _generatedExamQRepo;
+        private IInstructorCourseRepo _instructorCourseRepo;
+        private IGeneratedExamRepo _generatedExamRepo;
+        private ICourseDepartmentRepo _courseDepartmentRepo;
+        private ITopicRepo _topicRepo;
+        private ICourseTopicRepo _courseTopicRepo;
+        private IStudentCourseRepo _studentCourseRepo;
+        public IStudentRepo StudentRepo => _studentRepo ??= new StudentRepo(_db);
+        public IUserRepo UserRepo => _userRepo ??= new UserRepo(_db);
+        public IBranchRepo BranchRepo => _branchRepo ??= new BranchRepo(_db);
+        public IDepartmentRepo DepartmentRepo => _departmentRepo ??= new DepartmentRepo(_db);
+        public IInstructorRepo InstructorRepo => _instructorRepo ??= new InstructorRepo(_db);
+        public IUserTypeRepo UserTypeRepo => _userTypeRepo ??= new UserTypeRepo(_db);
+        public IExamRepo ExamRepo => _examRepo ??= new ExamRepo(_db);
+        public IDepartmentBranch DepartmentBranchRepo => _departmentBranchRepo ??= new DepartmentBranchRepo(_db);
+        public ICourseRepo CourseRepo => _courseRepo ??= new CourseRepo(_db);
+        public IExamQuestionRepo ExamQuestionRepo => _examQuestionRepo ??= new ExamQuestionRepo(_db);
+        public IGeneratedExamQRepo GeneratedExamQRepo => _generatedExamQRepo ??= new GeneratedExamQRepo(_db);
+        public IInstructorCourseRepo InstructorCourseRepo => _instructorCourseRepo ??= new InstructorCourseRepo(_db);
+        public IGeneratedExamRepo GeneratedExamRepo => _generatedExamRepo ??= new GeneratedExamRepo(_db);
+        public ICourseDepartmentRepo CourseDepartmentRepo => _courseDepartmentRepo ??= new CourseDepartmentRepo(_db);
+        public ITopicRepo TopicRepo => _topicRepo ??= new TopicRepo(_db);
+        public ICourseTopicRepo CourseTopicRepo => _courseTopicRepo ??= new CourseTopicRepo(_db);
+        public IStudentCourseRepo StudentCourseRepo => _studentCourseRepo ??= new StudentCourseRepo(_db);
+
 
         public UnitOfWork(AppDbContext db)
         {
 
             _db = db;
-            //StudentRepo = new StudentRepo(_db);
-            //UserRepo = new UserRepo(_db);
-            //BranchRepo = new BranchRepo(_db);
-            //DepartmentRepo = new DepartmentRepo(_db);
-            //InstructorRepo = new InstructorRepo(_db);
-            //UserTypeRepo = new UserTypeRepo(_db);
-            //DepartmentBranchRepo = new DepartmentBranchRepo(_db);
-            //UserTypeRepo = new UserTypeRepo(_db);
-            //ExamRepo = new ExamRepo(_db);
         }
         public int Save()
         {
