caaea1e077919375c9c36a575d05c6b42d09b6aa
Aseel-Elsawy <aseelelsawy7@gmail.com>
2025-04-28 16:03:28 +0300

    workning on reports

---
+++ Examination.PL/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,15 @@
+﻿using Examination.PL.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examination.PL.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [UserTypeAuthorize(Constants.UserTypes.Admin)]
+    public class ReportController : Controller
+    {
+        public IActionResult GetTopics()
+        {
+            return View();
+        }
+    }
+}
